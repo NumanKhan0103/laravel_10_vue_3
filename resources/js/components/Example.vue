11c1cc444ffd6f851313abb118e7d7ae547efcfe
numankhan <numankhannh405@gmail.com>
2023-09-11 16:39:43 +0500

    vue 3 install with larvel 10 and create a example component and render in welcome page

---
+++ resources/js/components/Example.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1> Example Component form Vue</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
